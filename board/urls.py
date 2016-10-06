52420910d4961daec300a9da1c8702e7a3a5d591
Michael Aldridge <aldridge.mac@gmail.com>
2016-10-06 13:25:21 -0500

    Add links bar at the bottom

--- board/urls.py
+++ board/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='board'),
     url(r'^tap-in/([\d]*)$', views.tap_in),
     url(r'^tap-out/([\d]*)$', views.tap_out),
     url(r'^log$', views.log),
